9614982cd58be5a0c363d7a11ff439e7074d508f
ishizakit <ishizakit@DESKTOP-7R4CSIA>
2019-10-21 19:35:01 +0900

    学習用プロジェクトをコミット

---
+++ src/main/java/jp/co/systena/tigerscave/shoppingcart/Problem1Application.java
@@ -0,0 +1,13 @@
+package jp.co.systena.tigerscave.shoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Problem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Problem1Application.class, args);
+	}
+
+}
