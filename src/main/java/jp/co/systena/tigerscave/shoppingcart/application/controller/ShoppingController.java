c9018b66ae0033c4389ce447b93ea8f53c5a0e6c
ishizakit <ishizakit@DESKTOP-7R4CSIA>
2019-10-22 12:59:00 +0900

    [fix]課題1-①商品一覧の渡し方を修正
    商品一覧情報の渡し方が詳細設計書と違っていたので修正
    Viewのテンプレートの設定の方法も違っていたので修正

--- src/main/java/jp/co/systena/tigerscave/shoppingcart/application/controller/ShoppingController.java
+++ src/main/java/jp/co/systena/tigerscave/shoppingcart/application/controller/ShoppingController.java
@@ -1,23 +1,21 @@
 package jp.co.systena.tigerscave.shoppingcart.application.controller;
 
 import java.util.Map;
-import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 import jp.co.systena.tigerscave.shoppingcart.application.service.ListService;
 
 @Controller
 public class ShoppingController {
   @RequestMapping("/List")
-  public String index(HttpSession session, Model model,
-      @RequestParam(name = "name", required = false) String name) {
+  public ModelAndView list(ModelAndView mav) {
     ListService list = new ListService();
-    Map itme_list = list.getItemList();
-    model.addAttribute("item_list", itme_list);
+    Map item_list = list.getItemList();
 
+    mav.addObject("ItemList", item_list);
+    mav.setViewName("ListView");
 
-    return "ListView";
+    return mav;
   }
 }
