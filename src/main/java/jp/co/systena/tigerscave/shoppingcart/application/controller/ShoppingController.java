310ea03df5e4aea209ae0ed9c36a736f0a597f8b
ishizakit <ishizakit@DESKTOP-7R4CSIA>
2019-10-22 21:22:08 +0900

    [add] 課題1-②を実装完了
    一通りの動作確認を行った

--- src/main/java/jp/co/systena/tigerscave/shoppingcart/application/controller/ShoppingController.java
+++ src/main/java/jp/co/systena/tigerscave/shoppingcart/application/controller/ShoppingController.java
@@ -1,14 +1,21 @@
 package jp.co.systena.tigerscave.shoppingcart.application.controller;
 
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
+import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import jp.co.systena.tigerscave.shoppingcart.application.model.Item;
+import jp.co.systena.tigerscave.shoppingcart.application.service.ListForm;
 import jp.co.systena.tigerscave.shoppingcart.application.service.ListService;
 
 @Controller
 public class ShoppingController {
-  @RequestMapping("/List")
+  @RequestMapping(value = "/List")
   public ModelAndView list(ModelAndView mav) {
     ListService list = new ListService();
     Map item_list = list.getItemList();
@@ -19,8 +26,25 @@ public class ShoppingController {
     return mav;
   }
 
-  @RequestMapping("/Order")
-  public ModelAndView order(ModelAndView mav) {
+
+  @RequestMapping(value = "/List", method = RequestMethod.POST)
+  public ModelAndView order(ModelAndView mav, @Valid ListForm listForm, BindingResult bindingResult) {
+    // 商品のIDが入っている
+    int item_id = listForm.getItemId();
+
+    // 商品のリストを取得する
+    ListService list_service = new ListService();
+    Map ls = list_service.getItemList();
+
+    // 探索
+    Set<Integer> MapSet = ls.keySet();
+    for(Iterator<Integer> n = MapSet.iterator(); n.hasNext();) {
+      Item i = (Item) ls.get(n.next());
+      if(item_id == i.getItemId()) {
+        mav.addObject("CartItem", i);
+        break;
+      }
+    }
 
     mav.setViewName("cartView");
 
---
+++ src/main/java/jp/co/systena/tigerscave/shoppingcart/application/service/ListForm.java
@@ -0,0 +1,32 @@
+package jp.co.systena.tigerscave.shoppingcart.application.service;
+
+public class ListForm {
+  private int itemId;
+  private String name;
+  private int price;
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getPrice() {
+    return this.price;
+  }
+
+  public void setPrice(int price) {
+    this.price = price;
+  }
+
+  public int getItemId() {
+    return itemId;
+  }
+
+  public void setItemId(int itemId) {
+    this.itemId = itemId;
+  }
+
+}
