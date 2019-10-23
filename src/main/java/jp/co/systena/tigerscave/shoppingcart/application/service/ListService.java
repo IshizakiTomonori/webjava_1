fd8f414470274c1688233bd71e318e03b8e0ee3f
ishizakit <ishizakit@DESKTOP-7R4CSIA>
2019-10-24 00:07:58 +0900

    [update] 課題1-①itemIdを変更
    自動で付与されているIDか、自分で設定したIDかの区別をつけやすくするためにIDを変更

--- src/main/java/jp/co/systena/tigerscave/shoppingcart/application/service/ListService.java
+++ src/main/java/jp/co/systena/tigerscave/shoppingcart/application/service/ListService.java
@@ -7,12 +7,12 @@ import jp.co.systena.tigerscave.shoppingcart.application.model.Item;
 public class ListService {
   public Map getItemList() {
     Map<Integer, Item> item_list = new HashMap<>();
-    Item item1 = new Item(1, "キーボード", 1000);
-    Item item2 = new Item(2, "ノートパソコン", 100000);
-    Item item3 = new Item(3, "マウス", 300);
-    item_list.put(1, item1);
-    item_list.put(2, item2);
-    item_list.put(3, item3);
+    Item item1 = new Item(10, "キーボード", 1000);
+    Item item2 = new Item(20, "ノートパソコン", 100000);
+    Item item3 = new Item(30, "マウス", 300);
+    item_list.put(10, item1);
+    item_list.put(20, item2);
+    item_list.put(30, item3);
     return item_list;
   }
 }
