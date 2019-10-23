a1358871b5e2774a7acdb969831b20423bac1582
ishizakit <ishizakit@DESKTOP-7R4CSIA>
2019-10-24 00:16:36 +0900

    [add] 課題1-③Orderクラスの追加
    詳細設計書に則ってOrderクラスを追加
    ほかのクラスや処理との紐づけは行っていない

---
+++ src/main/java/jp/co/systena/tigerscave/shoppingcart/application/model/Order.java
@@ -0,0 +1,15 @@
+package jp.co.systena.tigerscave.shoppingcart.application.model;
+
+public class Order {
+  private int itemId;
+
+  public int getItemId() {
+    return itemId;
+  }
+
+  public void setItemId(int itemId) {
+    this.itemId = itemId;
+  }
+
+
+}
